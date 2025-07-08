8c08ddc8ccc4ff8da42d149aa56c83a34edf5bad
Mahesh Tikar <125637707+tikarvishnu8@users.noreply.github.com>
2025-07-08 14:17:19 +0530

    Add files via upload

---
+++ Week 1 - Git-JS/Day2.js
@@ -0,0 +1,84 @@
+console.log("Operators and conditional statements");
+
+/*
+// Arithmetic operators
+
+let a = 10;
+let b = 20;
+
+console.log("a + b",a + b); //+ operators adding value.
+console.log("a - b",a - b); //- operators substracting value.
+console.log("a * b",a * b); //- operators multiply value.
+console.log("a / b",a / b); //- operators divide value.
+
+// Expnentional Operators
+
+console.log("a ** b", a ** b);
+
+// Increment operators, adding 1 and print the value and its a Post Increment Operators.
+console.log(++a)
+
+// Increment operators, First assign the value and then add and its a Pre Increment Operators.
+console.log(a++); // Its gives the same value 11 in case, after printing value a then add.
+console.log(a); // Here print 13 because we already add 1 in existing value.
+
+
+//Unary Operator
+// Increment operator ++
+// Decrement operator --
+
+// Assignment Operators
+// = , +=, -=,
+
+// Comprsion Opertors
+// ==, >=, <=, ===, !=, !==
+
+// Logical Opertors &&, ||, !   Logical operators only returns boolean values True or False.
+
+// Logical And
+
+let x = 10;
+let y = 9;
+
+let cond1 = x > y; //true
+let cond2 = x ===10; // true
+
+console.log("cond1 && cond2=", cond1 && cond2); // True in case. In && operators if both condition is true then only returns true either flase. 
+console.log("cond1 || cond2=", cond1 || cond2); // True in case. In || operators if one condition is true then returns true and if both are false then returns false.
+console.log(!(x<y)); //True in case. ! (Not) operators is print reverse values means true as a flase and flase as a true.
+
+// Conditional statements.
+console.log("Lets Learn Conditional Statements"); // To implement some conditions in the code.
+
+
+// If statements
+let age1 = 17;
+ if (age1 >= 18) { // When we have a condition we use if statements.
+    console.log("Yoy are adult");
+ }
+
+// If else statement
+
+if (age1 >= 18) { // in this case if age is 18 or equal 18 then its print you can vote if not then print else block.
+    console.log("You can vote"); 
+}else{
+    console.log("You cannot vote");
+}*/
+
+//if else if ladder
+
+let mode = "Blue";
+
+if (mode === "Dark") {
+    color = "black";
+}else if(mode === "Blue"){
+    color = "blue";
+}else if(mode === "Purple"){
+    color = "purple";
+}else{
+    color = "White";
+}
+console.log(color);
+
+
+console.log("hiii");
